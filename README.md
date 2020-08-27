269c15ce0f83c07816ca5fe448d6a293714d48af
Ghaith-Mgr <66948613+Ghaith-Mgr@users.noreply.github.com>
2020-08-27 16:09:59 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+Mon Package permet à n'impore quel utilisateur de faire des tests statistiques.
+Son Utilisation est simple.
+Plus de detail ici  
---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 27 16:03:12 2020
+
+@author: gmagroune
+"""
+
+from TestStat import PyTest
